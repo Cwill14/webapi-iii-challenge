bf003e07da96c9899f896eaa7b6134a7b21529ee
Clark Williams <theclarkwilliams@gmail.com>
2019-08-07 14:18:12 -0600

    logger middleware set up and working

--- server.js
+++ server.js
@@ -8,14 +8,18 @@ server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
-server.get('/', (req, res) => {
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
+  const method = req.method;
+  const url = req.url;
+  const timestamp = Date.now();
+  console.log(`${method} request to '${url}' at ${timestamp}`);
+  next()
 };
 
 module.exports = server;
