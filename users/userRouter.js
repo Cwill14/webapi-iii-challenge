45baa411eba49aba3455b9fba1ce4ffcbaea375d
Clark Williams <theclarkwilliams@gmail.com>
2019-08-07 15:29:47 -0600

    post and delete working, but working without any of the middleware yet

--- users/userRouter.js
+++ users/userRouter.js
@@ -5,7 +5,18 @@ const router = require('express').Router();
 const data = require('./userDb.js');
 
 router.post('/', (req, res) => {
-
+    const userBody = req.body
+    if (userBody.name) {
+        data.insert(userBody)
+            .then(resource => {
+                res.status(201).json(userBody)
+            })
+            .catch(error => {
+                res.status(500).json({ error: "error posting user" })
+            })
+    } else {
+        res.status(400)({ error: " missing name field" })
+    }
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -41,7 +52,18 @@ router.get('/:id/posts', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    // if (id) {
 
+    // }
+    data.remove(id)
+        .then(resource => {
+            // res.status(200).json({ message: "successfully deleted"})
+            res.status(200).json(resource)
+        })
+        .catch(error => {
+            res.status(500).json({ error: "server error deleting by ID" })
+        })
 });
 
 router.put('/:id', (req, res) => {
@@ -50,14 +72,15 @@ router.put('/:id', (req, res) => {
 
 //custom middleware
 
-// function validateUserId(req, res, next) {
-//     const { id } = req.params;
-//     if (id === ) {
-//         // store that user object as req.user
-//     } else {
-//         res.status(400).json({ message: "invalid user id" })
-//     }
-// };
+function validateUserId(req, res, next) {
+    const { id } = req.params;
+    if (id) {
+        req.user = { req}
+    } else {
+        res.status(400).json({ message: "invalid user id" })
+    }
+    next();
+};
 
 function validateUser(req, res, next) {
 
