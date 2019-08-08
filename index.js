c82f93054c421aa8fc58a2316b17edb2c3008bfb
Clark Williams <theclarkwilliams@gmail.com>
2019-08-08 13:36:23 -0600

    added .env and heroku elements

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // code away!
+require('dotenv').config();
 const server = require('./server.js');
 
-const port = 8000;
+// const port = 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => console.log(`\n*Server running on port ${port}*\n`))
--- server.js
+++ server.js
@@ -9,11 +9,13 @@ server.use(express.json());
 server.use(helmet());
 server.use(cors());
 server.use(logger)
-// server.get('/', logger, (req, res) => {
-//   res.send(`<h2>Let's write some middleware!</h2>`)
-// });
 
 server.use('/users', userRouter);
+server.get('/', (req, res) => {
+    res.status(200).json({
+      quoteOfTheDay: process.env.QOTD
+    })
+});
 
 //custom middleware
 
